7f9e0fd7b1f343eac2a64677177347afed95ac75
JingX <xin.jing1@thoughtworks.com>
2020-07-01 11:05:07 +0800

    [xin] p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTests.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber = 0")
+public class LevelControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnBasicWhenLevelNumberLessThanOne() throws Exception {
+        String result = mockMvc.perform(get("/level"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        assertEquals("basic", result);
+    }
+}
